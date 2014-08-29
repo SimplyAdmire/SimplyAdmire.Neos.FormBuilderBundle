99dde817405f11c8599eced58b8ff5576e6da7d0
Rens Admiraal <rens@rensadmiraal.nl>
2014-08-29 12:15:42 +0200

    [TASK] Add simple editor for confirmations

--- Resources/Public/Scripts/FormBuilder/finishers.js
+++ Resources/Public/Scripts/FormBuilder/finishers.js
@@ -6,4 +6,47 @@
 	Editor.FinisherEditor.FormBuilderBundleNodeRedirectFinisherEditor = Editor.ValidatorEditor.DefaultValidatorEditor.extend({
 		templateName: 'Finisher-FormBuilderBundleNodeRedirectEditor'
 	});
+
+	Editor.FinisherEditor.FormBuilderBundleConfirmationFinisherEditor = Editor.FinisherEditor.EmailFinisherEditor.extend({
+		templateName: 'Finisher-TYPO3FormConfirmationEditor',
+
+		EditButton: Ember.Button.extend({
+			tagName: 'a',
+			click: function(event) {
+				var that = this,
+					$editor = $('#dialog-confirmation-editor p.hallo-editor');
+
+
+				$editor.html(that.getPath('parentView.currentCollectionElement.options.message'));
+				$editor.hallo({
+					showAlways: true,
+					plugins: {
+						'halloformat': {},
+						'halloreundo': {},
+						'hallolink': {}
+					}
+				});
+
+				$('#dialog-confirmation-editor').dialog({
+					resizable: true,
+					height: 500,
+					width: 600,
+					modal: true,
+					buttons: {
+						Ok: function() {
+							that.setPath('parentView.currentCollectionElement.options.message', $('#dialog-confirmation-editor p.hallo-editor').html());
+							TYPO3.FormBuilder.Model.Form.set('unsavedContent', true);
+							$(this).dialog('close');
+						}
+					}
+				});
+				event.stopPropagation();
+				return false;
+			}
+		})
+	});
+
 })(TYPO3.FormBuilder.View.ElementOptionsPanel.Editor);
+
+
+$( "#dialog-confirm" )
